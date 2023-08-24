bd6de2cb296ee1e970e510fcd350868c9f34db33
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-08-24 14:36:09 +0300

    Gichachi component

---
+++ src/gichachi.js
@@ -0,0 +1,7 @@
+function Gichachi() {
+    return <div>
+        <p>I am currently a software engineering student in Moringa school. I love F1 and am an avid Manchester United Fan. I prefer smoked herbs over regular herbs.</p>
+    </div>
+}
+
+export default Gichachi
